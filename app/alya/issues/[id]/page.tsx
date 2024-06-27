963ba41d24972ab30c7d17eed422226de59269d2
laguagu <akuleskinen@hotmail.com>
2024-06-27 15:01:08 +0300

    refactor: issue page, add suspense loader

--- app/alya/issues/[id]/page.tsx
+++ app/alya/issues/[id]/page.tsx
@@ -5,68 +5,77 @@ import InformationCard from "@/components/issues/information-card";
 // import { fetchIssuePageData } from "@/data/mockDataFetch";
 import { fetchIssuePageData } from "@/lib/dataFetching";
 import BackButton from "@/components/issues/back-button";
+import { Suspense } from "react";
 
-export default async function Page({
+
+const AsyncDataComponent = async ({ issueId, deviceId }: { issueId: string, deviceId: string }) => {
+  const { issueData, deviceData, locationData, partsList } = await fetchIssuePageData(issueId, deviceId);
+
+  return (
+    <Tabs defaultValue="info">
+      <TabsList className="md:mb-2 ">
+        <TabsTrigger value="info" className="hover:font-semibold">
+          Tiedot
+        </TabsTrigger>
+        <TabsTrigger value="edit" className="hover:font-semibold">
+          Muokkaa
+        </TabsTrigger>
+        <TabsTrigger value="chat" className="hover:font-semibold">
+          Huolto-ohjeet
+        </TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="info">
+        <InformationCard
+          partsList={partsList}
+          issueData={issueData}
+          deviceData={deviceData}
+          locationName={locationData}
+        />
+      </TabsContent>
+
+      <TabsContent value="edit">
+        <ClientForm
+          data={issueData}
+          locationName={locationData}
+          deviceData={deviceData}
+        />
+      </TabsContent>
+
+      <TabsContent value="chat">Chatti.</TabsContent>
+    </Tabs>
+  );
+};
+
+const LoadingIndicator = () => <div>Ladataan vikailmoitusta...</div>;
+
+export default function Page({
   params,
   searchParams,
 }: {
   params?: { id?: string };
   searchParams: { [key: string]: string | undefined };
 }) {
-
-
   if (!params || !params.id) {
     return <div>Page ID is missing.</div>;
   }
   const deviceId = searchParams?.["device_id"];
   const issueId = params?.id.toString();
 
-  fetchIssuePageData(issueId, deviceId!);
-  const { issueData, deviceData, locationData, partsList } =
-    await fetchIssuePageData(issueId, deviceId!);
+  if (!deviceId) {
+    return <div>Device ID is missing.</div>;
+  }
 
   return (
     <div>
       <div className="flex items-center gap-2">
-      <BackButton />
-      <h1 className="text-2xl">Vikailmoitus - {params?.id}</h1>
+        <BackButton />
+        <h1 className="text-2xl">Vikailmoitus - {params?.id}</h1>
       </div>
       <Separator className="md:my-4 my-3 " />
-      <Tabs defaultValue="info">
-        <TabsList className="md:mb-2 ">
-          <TabsTrigger value="info" className="hover:font-semibold">
-            Tiedot
-          </TabsTrigger>
-          <TabsTrigger value="edit" className="hover:font-semibold">
-            Muokkaa
-          </TabsTrigger>
-          <TabsTrigger value="chat" className="hover:font-semibold">
-            Huolto-ohjeet
-          </TabsTrigger>
-        </TabsList>
-
-        {/* Inffo */}
-        <TabsContent value="info">
-          <InformationCard
-            partsList={partsList}
-            issueData={issueData}
-            deviceData={deviceData}
-            locationName={locationData}
-          />
-        </TabsContent>
-
-        {/* Edit */}
-        <TabsContent value="edit">
-          <ClientForm
-            data={issueData}
-            locationName={locationData}
-            deviceData={deviceData}
-          />
-        </TabsContent>
-
-        {/* Chat */}
-        <TabsContent value="chat">Chatti.</TabsContent>
-      </Tabs>
+      <Suspense fallback={<LoadingIndicator />}>
+        <AsyncDataComponent issueId={issueId} deviceId={deviceId} />
+      </Suspense>
     </div>
   );
 }
