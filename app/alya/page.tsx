ddfe5449821582ac3a25c0e768027cea31d52db9
laguagu <49702497+laguagu@users.noreply.github.com>
2024-07-01 13:12:48 +0300

    Add next safe actions

--- app/alya/page.tsx
+++ app/alya/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1 className="text-4xl">Dashboard</h1>
     </div>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,8 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-
 export async function pause(ms: number) {
   console.log("pause", ms)
   return new Promise(resolve => setTimeout(resolve, ms))
