7b1da9e0a1ff675c2bce2e62b23b5c9f5dd28aef
laguagu <49702497+laguagu@users.noreply.github.com>
2024-07-04 17:21:19 +0300

    fix: middlware add matcher function to reduce middleware calls

--- middleware.ts
+++ middleware.ts
@@ -31,3 +31,9 @@ export default async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
+
+export const config = {
+    matcher: [
+      '/((?!_next/static|_next/image|favicon.ico).*)', // Exclude static files and images
+    ],
+  };
