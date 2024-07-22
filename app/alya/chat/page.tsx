ca086e747f546f47a0c28b38a30e4a22b8a42578
laguagu <49702497+laguagu@users.noreply.github.com>
2024-07-22 22:38:16 +0300

    Remove random bracket

--- app/alya/chat/page.tsx
+++ app/alya/chat/page.tsx
@@ -1,8 +1,13 @@
 import { getSessionAction } from "@/lib/actions";
 import ChatComponent from "@/components/chat/chat-component";
+import { Suspense } from "react";
 
 export default async function Page() {
   const session = await getSessionAction();
   const sessionUserId = session ? session.userId : null;
-  return <ChatComponent initialSessionUserId={sessionUserId} />;
+  return (
+    <Suspense fallback={<div>Chatbot sivua ladataan...</div>}>
+      <ChatComponent initialSessionUserId={sessionUserId} />
+    </Suspense>
+  );
 }
