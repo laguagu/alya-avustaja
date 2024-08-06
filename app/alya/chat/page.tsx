c78655e34a1eebde6dec775b27f66a95c1dc1ae4
laguagu <49702497+laguagu@users.noreply.github.com>
2024-08-06 14:00:04 +0300

    Add chatbot skeleton

--- app/alya/chat/page.tsx
+++ app/alya/chat/page.tsx
@@ -1,14 +1,13 @@
 import ChatComponent from "@/components/chat/chat-component";
 import { Suspense } from "react";
 import { verifySession } from "@/app/_auth/sessions";
+import { ChatSkeletton } from "@/components/skeletons";
 
 export default async function Page() {
   const session = await verifySession();
   const sessionUserId = session ? session.userId : null;
   return (
-    <Suspense
-      fallback={<div className="font-bold">Chatbot sivua ladataan...</div>}
-    >
+    <Suspense fallback={<ChatSkeletton />}>
       <ChatComponent initialSessionUserId={sessionUserId} />
     </Suspense>
   );
--- components/skeletons.tsx
+++ components/skeletons.tsx
@@ -32,4 +32,39 @@ export function LoadingIssuePageSkeleton() {
 }
 
 // ChatSkeletton: Alya/Chat
+
+export function ChatSkeletton() {
+  return (
+    <div className="flex flex-col w-full flex-grow max-h-dvh">
+      <div className="p-4 w-full max-w-3xl mx-auto">
+        <div className="flex items-center">
+          <h1 className="text-md text-nowrap sm:text-2xl lg:text-3xl font-bold text-center flex-1">
+            Chatbot - Älyäavustaja
+          </h1>
+        </div>
+      </div>
+      <section className="container px-0 pb-10 flex flex-col flex-grow gap-4 mx-auto max-w-3xl shadow-sm border rounded-b-lg">
+        <div className="p-4 flex-grow bg-muted/50 rounded-lg overflow-y-auto flex flex-col gap-4">
+          <Skeleton className="h-16 w-3/4 self-start" />
+          <Skeleton className="h-16 w-3/4 self-end" />
+          <Skeleton className="h-16 w-2/3 self-start" />
+          <Skeleton className="h-16 w-2/3 self-end" />
+        </div>
+      </section>
+      <section className="p-4">
+        <div className="flex w-full flex-col sm:flex-row max-w-3xl mx-auto items-center space-y-2 sm:space-y-0">
+          <Skeleton className="flex-1 h-12" />
+          <div className="flex gap-2 items-center mt-2 sm:mt-0">
+            <Skeleton className="h-12 w-28 ml-2" />
+            <Skeleton className="h-12 w-44" />
+          </div>
+        </div>
+      </section>
+      <div className="flex flex-col items-center justify-center mt-4">
+        <Skeleton className="h-4 w-96 mb-2" />
+        <Skeleton className="h-10 w-48" />
+      </div>
+    </div>
+  );
+}
 // FurnituresSkeleton: Alya/Furnitures
