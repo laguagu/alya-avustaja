b47cab1bdbf8d6ba8a812f80b27c665e308032b1
laguagu <49702497+laguagu@users.noreply.github.com>
2024-08-20 21:09:23 +0300

    Style: Add alert text for user

--- app/alya/issues/new-issue/page.tsx
+++ app/alya/issues/new-issue/page.tsx
@@ -2,6 +2,8 @@ import NewIssueForm from "@/components/issues/newissue-form";
 import { Suspense } from "react";
 import BackButton from "@/components/issues/back-button";
 import { Separator } from "@/components/ui/separator";
+import { TextEffect } from "@/components/ui/text-effect";
+import { AlertTriangleIcon } from "lucide-react";
 
 export default function Page() {
   return (
@@ -10,6 +12,12 @@ export default function Page() {
         <div className="flex items-center gap-2 mb-4">
           <BackButton />
           <h1 className="text-2xl">Uusi vikailmoitus</h1>
+          <div className="flex sm:gap-3 ">
+            <AlertTriangleIcon className="h-6 w-6 text-red-500" />
+            <TextEffect per="word" preset="slide" className="text-red-500">
+              Mahdollinen ominaisuus jota voi kokeilla. Ei vielä käytössä
+            </TextEffect>
+          </div>
         </div>
         <Separator className="md:mb-6 mb-4" />
       </div>
